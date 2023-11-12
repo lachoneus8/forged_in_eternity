aee8fcff143b28618197c75caed863172aba0060
BlazeDrake <blaze.drake@outlook.com>
2023-11-11 18:47:46 -0700

    SetBaseHealth

--- Assets/Scripts/Gameplay/Enemies/Boss.cs
+++ Assets/Scripts/Gameplay/Enemies/Boss.cs
@@ -9,15 +9,32 @@ public class Boss : EnemyBase
     public List<BossAttack> attacks;
     public float cooldown;
     public Projectile projectile;
-    public float turnSpeed;
-
+    public float targetDist;
+    public float distBuffer = 0.5f;
+    public CharacterController characterController;
+    bool isAttacking = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (cooldown <= 0)
+        if (cooldown <= 0&&!isAttacking)
         {
+            int newAttack=UnityEngine.Random.Range(0,attacks.Count);
+            StartCoroutine(Attack(attacks[newAttack]));
+        }
+        else if (!isAttacking)
+        {
+            cooldown -= Time.deltaTime;
+        }
 
+        LookAtPlayer(turnSpeed*Time.deltaTime);
+        if(Vector3.Distance(transform.position, player.transform.position) > targetDist+distBuffer)
+        {
+            characterController.Move(transform.forward*Time.deltaTime);
+        }
+        else if (Vector3.Distance(transform.position, player.transform.position) < targetDist - distBuffer)
+        {
+            characterController.Move(transform.forward * -1 * Time.deltaTime);
         }
     }
 
@@ -37,24 +54,35 @@ public class Boss : EnemyBase
 
         public float rangedSpeed;
         public float rangedSize;
+        public Color projectileColor;
 
         public Collider meleeCollider;
     }
 
     IEnumerator Attack(BossAttack attack)
     {
+        isAttacking = true;
+        targetDist = (attack.maxDist + attack.minDist) / 2.0f;
+        //yield return new WaitUntil(() => Vector3.Distance(transform.position, player.transform.position) > attack.minDist && Vector3.Distance(transform.position, player.transform.position) < attack.maxDist);
         SetColor(attackColor);
         yield return new WaitForSeconds(attack.delay);
-        while (attack.repeats > 0)
+        for (int i=0;i<attack.repeats;i++)
         {
             if(attack.isRanged)
             {
-                Projectile spawnedProjectile=Instantiate(projectile, Vector3.zero, Quaternion.identity);
-                projectile.speed = attack.rangedSpeed;
-                projectile.damage = attack.damage;
-                projectile.transform.localScale *= attack.rangedSize;
+                var spawnPos = transform.position;
+                spawnPos.y = player.transform.position.y;
+                Projectile spawnedProjectile=Instantiate(projectile, spawnPos, transform.rotation);
+                spawnedProjectile.speed = attack.rangedSpeed;
+                spawnedProjectile.damage = attack.damage;
+                //spawnedProjectile.transform.localScale *= attack.rangedSize;
+                spawnedProjectile.GetComponent<MeshRenderer>().material.color = attack.projectileColor;
             }
+
+            yield return new WaitForSeconds(attack.repeatDelay);
         }
         SetColor(defaultColor);
+        cooldown = attack.cooldown;
+        isAttacking = false;
     }
 }
